dc22fdd3c22a289c44f501d7a935a5dc10c4548e
Matt Rumschlag <matt.rumschlag@ey.com>
2021-10-07 23:06:09 -0400

    first commit

---
+++ lib/commands/metadata/profiles/split.d.ts
@@ -0,0 +1,13 @@
+import { core, flags, SfdxCommand } from '@salesforce/command';
+export default class Split extends SfdxCommand {
+    static description: string;
+    static examples: string[];
+    protected static flagsConfig: {
+        input: flags.IOptionFlag<string>;
+        output: flags.IOptionFlag<string>;
+        delete: import("../../../../../../../../Users/RJ857MQ/projects/sfdx-profiles-splitter/node_modules/@salesforce/command/node_modules/@oclif/command/node_modules/@oclif/parser/lib/flags").IBooleanFlag<boolean>;
+    };
+    protected static requiresProject: boolean;
+    split(inputDir: string, outputDir: string, deleteProfile: boolean): Promise<any>;
+    run(): Promise<core.AnyJson>;
+}
